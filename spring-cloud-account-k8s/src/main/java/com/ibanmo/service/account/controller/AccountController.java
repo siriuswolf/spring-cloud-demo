ecfe01aefad14459e48b19a4476f9d24dbc85db7
zhangyh <zhangyh@pikaent.com>
2020-08-29 11:40:13 +0800

    add rest api

---
+++ spring-cloud-account-k8s/src/main/java/com/ibanmo/service/account/controller/AccountController.java
@@ -0,0 +1,23 @@
+package com.ibanmo.service.account.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AccountController {
+    private static final Log log = LogFactory.getLog(AccountController.class);
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/services")
+    public List<String> services() {
+        return this.discoveryClient.getServices();
+    }
+}
